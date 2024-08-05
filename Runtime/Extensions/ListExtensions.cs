b6e053d618c72da9b7546437c6fa3faaa17adc98
loykasi <loykasdanhcn@gmail.com>
2024-08-05 20:02:28 +0700

    Add Mathf extensions

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,11 +12,27 @@ namespace GameCore.Extensions
             return Random.Range(0, list.Count);
         }
 
+        public static int GetRandomIndex<T>(this List<T> list, int length)
+        {
+            if (list.Count == 0)
+                return -1;
+            length = Mathf.Min(length, list.Count);
+            return Random.Range(0, length);
+        }
+
         public static T GetRandom<T>(this List<T> list)
         {
             if (list.Count == 0)
                 return default;
             return list[Random.Range(0, list.Count)];
+        }
+
+        public static T GetRandom<T>(this List<T> list, int length)
+        {
+            if (list.Count == 0)
+                return default;
+            length = Mathf.Min(length, list.Count);
+            return list[Random.Range(0, length)];
         }
     }   
 }
